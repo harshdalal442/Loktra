4635346616e98cf6eb92bec70348e36e69bbb5c9
olofmeister <harsh.d.btechi14@ahduni.edu.in>
2018-06-13 10:15:01 +0530

    get html done

--- WebCrawler/crawler.py
+++ WebCrawler/crawler.py
@@ -1,12 +1,23 @@
 # Web Crawler
-
 from bs4 import BeautifulSoup
 import requests
 import sys
 
+def generate_url(keyword, page_number=None):
+	if page_number is not None:
+		return 'http://www.shopping.com/products~PG-'+page_number+'?KW='+keyword
+	else:
+		return 'http://www.shopping.com/products?KW='+keyword
+
+def get_html(url):
+	response = requests.get(url)
+	if (response.status_code == 200):
+		return response.text
+	else:
+		print("Server under maintenance.! Please try again later")
 
 def crawl_keyword(keyword):
-	print(keyword)	
+	html = get_html(generate_url(keyword))	
 	return keyword
 
 def crawl_keyword_page(keyword, page_number):
