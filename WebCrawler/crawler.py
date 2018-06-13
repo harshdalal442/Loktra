97787aeb3ded4588ad2fc255dce5821eb4a1cb37
olofmeister <harsh.d.btechi14@ahduni.edu.in>
2018-06-13 10:52:55 +0530

    Web Crawler done

--- WebCrawler/crawler.py
+++ WebCrawler/crawler.py
@@ -17,10 +17,23 @@ def generate_url(keyword, page_number=None):
 	else:
 		return 'http://www.shopping.com/products?KW='+keyword
 
-def parse_html(response):
+def parse_html_total_number_of_results(response):
 	soup = BeautifulSoup(response, "html.parser")
 	spans = soup.find_all('span', attrs={'class':'numTotalResults'})
 	return ("".join(spans[0].strings))
+
+def parse_html_get_results(response):
+	soup = BeautifulSoup(response, "html.parser")
+	spans = soup.find_all('span', attrs={'class':'quickLookGridItemFullName hide'})
+	atags = soup.find_all('span', attrs={'class':'productPrice'})	
+
+	if len(spans) == 0:
+		print("There are no results available for this page number")
+	else:
+		for i in range(len(spans)):		
+			print("Product Name: ", ("".join(spans[i].strings)))
+			print("Product Price: ", ("".join(atags[i].strings)).replace("\n",""))
+			print("\n")		
 
 def get_html(url):
 	response = requests.get(url)	
@@ -31,15 +44,19 @@ def get_html(url):
 
 def crawl_keyword(keyword):
 	html = get_html(generate_url(keyword))	
-	print("Number of total results for "+keyword+" are: ", get_maximum_number(parse_html(html)))
+	total_number_of_results = get_maximum_number(parse_html_total_number_of_results(html))
+	print("Number of total results for "+keyword+" are: ", total_number_of_results)
 
 def crawl_keyword_page_number(keyword, page_number):
 	html = get_html(generate_url(keyword, page_number))	
-	return (keyword, page_number)
+	parse_html_get_results(html)
 
 if len(sys.argv) == 2:
 	crawl_keyword(sys.argv[1])
 elif len(sys.argv) == 3:
-	crawl_keyword_page_number(sys.argv[1], sys.argv[2])
+	if(not(sys.argv[2].isdigit())):
+		print("Please enter page_number as a number.")
+	else:
+		crawl_keyword_page_number(sys.argv[1], sys.argv[2])
 else:
 	print("Please run the code using one of the below:\n  --> python crawler.py <keyword> \n  --> python crawler.py <keyword> <page number>")
